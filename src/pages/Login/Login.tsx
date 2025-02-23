cd89851445c5902aac99dd06d2d9eb3c0dfb8b4d
AhmedToldiev <toldiev.009@mail.ru>
2025-02-23 13:19:47 +0300

    обработали ошибку через extrareducers

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -2,14 +2,11 @@ import { Link, useNavigate } from 'react-router-dom';
 import Button from '../../components/Button/Button';
 import Input from '../../components/Input/Input';
 import styles from './Login.module.css';
-import { FormEvent, use, useState } from 'react';
-import axios, { AxiosError } from 'axios';
-import { PREFIX } from '../../helpers/API';
+import { FormEvent, useEffect } from 'react';
 import Heading from '../../components/Heading/Heading';
-import { LoginResponse } from '../../interfaces/auth.interface';
-import { AppDispatch } from '../../store/store';
-import { useDispatch } from 'react-redux';
-import { userActions } from '../../store/user.slice';
+import { AppDispatch, RootState } from '../../store/store';
+import { useDispatch, useSelector } from 'react-redux';
+import { login, userActions } from '../../store/user.slice';
 
 export type LoginForm = {
 	email: {
@@ -21,38 +18,34 @@ export type LoginForm = {
 };
 
 export function Login() {
-	const [error, setError] = useState<string | null>();
 	const navigate = useNavigate();
 	const dispatch = useDispatch<AppDispatch>();
+	const { jwt, loginErrorMessage } = useSelector(
+		(state: RootState) => state.user
+	);
+
+	useEffect(() => {
+		if (jwt) {
+			navigate('/');
+		}
+	}, [jwt, navigate]);
 
 	const submit = async (e: FormEvent) => {
 		e.preventDefault();
-		setError(null);
+		dispatch(userActions.clearLoginError());
 		const target = e.target as typeof e.target & LoginForm;
 		const { email, password } = target;
 		await sendLogin(email.value, password.value);
 	};
 
 	const sendLogin = async (email: string, password: string) => {
-		try {
-			const { data } = await axios.post<LoginResponse>(`${PREFIX}/auth/login`, {
-				email,
-				password,
-			});
-			console.log(data);
-			dispatch(userActions.addJwt(data.access_token));
-			navigate('/');
-		} catch (e) {
-			if (e instanceof AxiosError) {
-				setError(e.response?.data.message);
-			}
-		}
+		dispatch(login({ email, password }));
 	};
 
 	return (
 		<div className={styles['login']}>
 			<Heading>Вход</Heading>
-			{error && <div className={styles['error']}>{error}</div>}
+			{loginErrorMessage && <div className={styles['error']}>{loginErrorMessage}</div>}
 			<form className={styles['form']} onSubmit={submit}>
 				<div className={styles['field']}>
 					<label htmlFor='email'>Ваш email</label>
